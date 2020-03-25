8a87eed0cb1bd78cffcd32eddaee722f5816911b
Sara Laffin <sarala@umich.edu>
2020-03-25 16:06:06 -0400

    fix bug in urls and serializers. CRUD up and running!

--- library/serializers.py
+++ library/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id',title, author, genre)
+        fields = ('id','title', 'author', 'genre')
