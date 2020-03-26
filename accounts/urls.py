f95f84b1ce72e0a125f287b4b327f33d5f2a7d86
Sara Laffin <sarala@umich.edu>
2020-03-26 15:42:15 -0400

    add login path to accounts.urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,7 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
-         name='token_refresh'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/login/', views.LoginView.as_view(), name="auth-login"),
 ]
