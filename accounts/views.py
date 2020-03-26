6b735980f440e74f50537fdd231d2d5b503c15e5
Sara Laffin <sarala@umich.edu>
2020-03-26 16:32:59 -0400

    postman working!

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 # Here, we are adding all of the necessary imports for our LoginView
 from django.shortcuts import render, redirect
-from rest_framework import generics, permissions, status, Response
+from rest_framework import generics, permissions, status, response
 from .serializers import UserSerializer, TokenSerializer
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
@@ -35,5 +35,29 @@ class LoginView(generics.ListCreateAPIView):
                 "token": str(refresh.access_token)
                 })
             serializer.is_valid()
-            return Response(serializer.data)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return response.Response(serializer.data)
+        return response.Response(status=status.HTTP_401_UNAUTHORIZED)
+
+class RegisterUsersView(generics.ListCreateAPIView):
+    """
+    POST user/signup/
+    """
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        email = request.data.get("email", "")
+        if not username and not password and not email:
+            return response.Response(
+                data={
+                    "message": "username, password and email is required to register a user"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
+        return response.Response(status=status.HTTP_201_CREATED)
--- library/views.py
+++ library/views.py
@@ -5,10 +5,14 @@ from rest_framework import generics
 
 from .models import Book
 
+from rest_framework import permissions
+
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (permissions.IsAuthenticated,)
